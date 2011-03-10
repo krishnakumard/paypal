94fc799ab54cab837286f914a7cecc0dca614403
Krishna kumar <krishna@xtrafin.net>
2011-03-10 15:02:07 +0530

    my initial commit

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,54 @@
+class PaymentsController < ApplicationController
+
+  include ActiveMerchant::Billing
+  
+  def index    
+  end
+
+  def confirm
+    redirect_to :action => 'index' unless params[:token]
+
+    details_response = gateway.details_for(params[:token])
+
+    if !details_response.success?
+      @message = details_response.message
+      render :action => 'error'
+      return
+    end
+
+    @address = details_response.address
+  end
+
+  def complete
+    purchase = gateway.purchase(1000,
+    :ip       => request.remote_ip,
+    :payer_id => params[:payer_id],
+    :token    => params[:token])
+
+    if !purchase.success?
+      @message = purchase.message
+      render :action => 'error'
+      return
+    end
+  end
+
+  def checkout
+    setup_response = gateway.setup_purchase(1000,
+      :ip                => request.remote_ip,
+      :return_url        => url_for(:action => 'confirm', :only_path => false),
+      :cancel_return_url => url_for(:action => 'index', :only_path => false)
+    )
+    redirect_to gateway.redirect_url_for(setup_response.token)
+  end
+
+
+  private
+
+  def gateway
+    @gateway ||= PaypalExpressGateway.new(
+      :login => 'seller_1299480946_biz_api1.xtrafin.net',
+      :password => 'GNDW8XHEQZF66ZW8',
+      :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31AdHk-qq0fhZ3u4tDgvK87wxd5nOL')
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paypal::Application.config.secret_token = 'ac50e2f566f730eb510822fde41e22135bf9e2648e9e9ea65faa71874ad08c89bc1b19231b1ee0450e0252dbdeb35f1d99ccd46072a4ced239eeacedbb5dbd6a'
