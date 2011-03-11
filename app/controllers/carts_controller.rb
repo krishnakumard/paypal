0201d1bf4f78fe0c222d0ac36ee2b267b7cbf4e3
Krishna kumar <krishna@xtrafin.net>
2011-03-11 10:54:17 +0530

    Cart and paypal

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -77,13 +77,5 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.xml
   def destroy
-    @cart = Cart.find(params[:id])
-    @cart.destroy
-    session[:cart_id] = nil
-    respond_to do |format|
-      format.html { redirect_to(store_url, :notice => 'Your cart is currently empty') }
-      format.xml { head :ok }
-    end
   end
-
 end
